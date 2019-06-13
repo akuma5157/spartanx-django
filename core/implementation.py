adb1e24bda2e6a7b0a1f5d7866b2fbda8deca359
Ajay Kumar <ajay.t.kumar@capgemini.com>
2019-06-14 00:06:28 +0530

    added implementation

---
+++ core/implementation.py
@@ -0,0 +1,20 @@
+import os
+
+from ruamel.yaml import YAML
+
+from .stubs import AbstractStubClass
+
+yaml = YAML()
+yaml.explicit_start = True
+yaml.indent(sequence=4, offset=2)
+
+
+class Implementation(AbstractStubClass):
+
+    def getSpec(request, *args, **kwargs):
+        try:
+            with open(os.path.join("swagger-spec.yml"), "r") as f:
+                spec = yaml.load(f)
+        except EnvironmentError:
+            spec = "No Swagger Spec available"
+        return spec
