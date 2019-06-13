76a9b8d2a63783f5e4e02d22404ae211439a289e
Ajay Kumar <ajay.t.kumar@capgemini.com>
2019-06-14 03:07:14 +0530

    serving app on root url

--- spartanx-django/urls.py
+++ spartanx-django/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path(r'^core/', include('core.urls')),
+    re_path(r'^', include('core.urls')),
     # re_path(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
