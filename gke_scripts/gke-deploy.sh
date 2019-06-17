e382f012f2d23d9cd2f04c6b954cddadd74a8eb4
Ajay <ajay.kumar.19495@gmail.com>
2019-06-18 02:52:50 +0530

    fixed travis.yml - hopefully

--- gke_scripts/gke-deploy.sh
+++ gke_scripts/gke-deploy.sh
@@ -4,4 +4,4 @@ echo "GKE DEPLOY SCRIPT COMES HERE"
 
 gcloud container clusters get-credentials "$GCLOUD_CLUSTER_NAME" --zone="$GCLOUD_ZONE"
 
-kubectl set image deployment $1 django=$2:$3
+$GCLOUD_HOME/bin/kubectl set image deployment $1 django=$2:$3
