ee9c4f5678efdfd68f9ebbc9ca973f03eefab9f6
Ajay <ajay.kumar.19495@gmail.com>
2019-06-18 02:27:58 +0530

    fixed travis.yml - hopefully

--- gke_scripts/gcloud-install.sh
+++ gke_scripts/gcloud-install.sh
@@ -14,14 +14,7 @@ if [ ! -f "$GCLOUD_PATH_APPLY" ]; then
   echo Installing gcloud SDK
   rm -rf "$GCLOUD_HOME"
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1 # SDK installation is interactive, thus prompts must be disabled
-    # Add the Cloud SDK distribution URI as a package source
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-    # Import the Google Cloud Platform public key
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-
-    # Update the package list and install the Cloud SDK
-    sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+  curl "https://sdk.cloud.google.com" | bash > /dev/null
 fi
 
 . "$GCLOUD_PATH_APPLY"
