dc9f21ed58d8d0fb2caf9131acd7eafbd97015d0
Ajay <ajay.kumar.19495@gmail.com>
2019-06-18 08:56:01 +0530

    Fixed Travis

--- gke_scripts/gke-deploy.sh
+++ gke_scripts/gke-deploy.sh
@@ -2,6 +2,8 @@
 
 echo "GKE DEPLOY SCRIPT COMES HERE"
 
+echo "SETTING UP KUBECONFIG"
 gcloud container clusters get-credentials "$GCLOUD_CLUSTER_NAME" --zone="$GCLOUD_ZONE"
 
+echo "UPDATING IMAGE IN DEPLOYMENT"
 $GCLOUD_HOME/bin/kubectl set image deployment $1 django=$2:$3
